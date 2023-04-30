3ebc74d21d3e1c20715525ee610440b85f51bca3
lolstershwjav <50257459+lolstershwjav@users.noreply.github.com>
2023-04-30 11:47:26 -0700

    Add files via upload

---
+++ c115project.py
@@ -0,0 +1,13 @@
+import sys
+
+import os
+
+
+
+
+path = "/Users/abhijoy/Desktop/Visual Studio: Coding/Python/Byjus Class/C115-boilerplate/bcd.txt"
+source = "bcd.txt"
+
+destination = "abc.txt"
+
+result = os.rename(path, destination)
